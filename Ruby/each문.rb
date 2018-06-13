0eab1235f0c77f3e195e0362a1a7ce2dba5ebf1a
kyueunQ <35055662+kyueunQ@users.noreply.github.com>
2018-06-14 00:43:09 +0900

    each 문 / (each문 활용) 문장 속의 단어 빈도 수 측정
    # codecademy Ruby

--- Ruby/each문.rb
+++ Ruby/each문.rb
@@ -27,3 +27,37 @@ dinner_menu = {
 dinner_menu.each do |guest, menu|
   puts "#{menu}"
 end
+
+
+
+# 3. each do & .sort_by do를 활용한 단어 빈도 수 측정하기
+puts "Type messages : "
+text = gets.chomp
+
+words = text.split
+
+# hash를 생성 (default value 값 = 0)
+frequencies = Hash.new(0)
+
+# key값이 word로 value값은 현재 0
+words.each do |word| 
+  frequencies[word] += 1
+end
+
+# hash 형태로 데이터가 저장되어 있음
+print frequencies
+
+# hash를 배열로 정돈함
+frequencies = frequencies.sort_by do |letter, count|
+  # sort_by를 통해 오름차순 정렬을 하는데, letter 기준으로 할지, count 기준으로 할지 정하기
+  count
+end
+# 내림차순 정렬
+frequencies.reverse!
+
+# 배열 형태로 데이터가 저장되어 있음
+print frequencies
+
+frequencies.each do |letter, count|
+  puts letter + " " + count.to_s
+end
